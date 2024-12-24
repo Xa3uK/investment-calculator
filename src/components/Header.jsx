0d19f91f47e4c29851363dc65b8cf2761b28f2c1
volodymyr.koval <hazardsales@gmail.com>
2024-12-24 22:18:40 +0100

    add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+      <div id="header">
+          <img src="/investment-calculator-logo.png" alt="Logo"/>
+          <h1>Investment calculator</h1>
+      </div>
+  );
+}
+
+export default Header;
